6ef485c38099ebc0c1a86a0adcbd71137a5e9aaa
oskataboska <biplo@interia.pl>
2021-07-15 00:53:10 +0200

    Equipment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About"
 import References from "./components/References";
+import Equipment from "./components/Equipment";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <About/>
       <References/>
+      <Equipment/>
     </div>
   );
 }
---
+++ src/components/Equipment.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./equipment.css"
+const Equipment = () => {
+    return (
+<section className="equipment-container">
+            <div className="right">
+                <div className="equipment-title">
+                <h1>Sprzęt</h1>
+                <div class='rect rect1'></div>
+                <div class='rect rect2'></div>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat repellendus assumenda corporis illo! Repellat accusamus perspiciatis inventore dolores, eligendi asperiores. Labore autem ut, recusandae architecto iure nihil maxime fugiat id necessitatibus temporibus, ratione at nulla, earum natus perferendis. Dolore voluptatibus quibusdam aspernatur nemo quidem aperiam placeat labore quam? Tempore, doloribus!</p>
+            </div>
+            <div className="left">
+            </div>
+            </section>
+    )
+}
+
+export default Equipment
