6d5da2762be2e42a2fef97054bf250cf446d9f95
oskataboska <87379001+oskataboska@users.noreply.github.com>
2021-07-19 13:46:29 +0200

    logo-title-added-bug-fixed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
             <li><FontAwesomeIcon className="fontAwesomeIcon" icon={faEnvelope}/><span><p>Właściciel firmy:</p> <a href="mailto:andrzej.borzek@a-tel.com.pl">andrzej.borzek@a-tel.com.pl</a></span></li>
             <li><FontAwesomeIcon className="fontAwesomeIcon" icon={faEnvelope}/><span><p>Asystentka:</p> <a href="mailto:agnieszka.pierzga@a-tel.com.pl">agnieszka.pierzga@a-tel.com.pl</a></span></li>
             <li><FontAwesomeIcon className="fontAwesomeIcon" icon={faEnvelope}/><span><p>Biuro:</p> <a href="mailto:biuro@a-tel.com.pl">biuro@a-tel.com.pl</a></span></li>
-            <li><FontAwesomeIcon className="fontAwesomeIcon" icon={faHome}/><span>Wolności 99, 45-920 Opole</span></li>
+            <li><FontAwesomeIcon className="fontAwesomeIcon" icon={faHome}/><span><a href = "https://www.google.com/maps/place/Wolno%C5%9Bci+99,+46-020+Opole/@50.737193,17.9085862,17z/data=!3m1!4b1!4m5!3m4!1s0x471051e63140a8f5:0xe62d534ba8576e6!8m2!3d50.737193!4d17.9107749" target="_blank">Wolności 99, 45-920 Opole</a></span></li>
           </ul>
         </div>
         <div className="contact-form">
